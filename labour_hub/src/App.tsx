bfb6e81bb170a8b231933b07e93a63f25d356320
Suhani Kumari <suhani0472@gmail.com>
2026-01-17 12:53:14 +0530

    labour register form

--- labour_hub/src/App.tsx
+++ labour_hub/src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Dashboard from "./pages/Dashboard";
 import FindLabour from "./pages/FindLabour";
-
+import Register from "./pages/LabourRegister";
 function App() {
   return (
     <>
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/find-labour" element={<FindLabour />} />
+        <Route path="/register" element={<Register />} /> 
       </Routes>
       <Footer />
     </>
